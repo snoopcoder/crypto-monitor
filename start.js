9f14107fe26f7ff48fedbb6a09ae759680b6ff1f
scode <scode@bk.ru>
2018-03-28 22:34:55 +0700

    продолжаем

--- start.js
+++ start.js
@@ -2,12 +2,19 @@ var zabbixNode = require("zabbix-node")
 var client = new zabbixNode('http://zbxs.e5-nsk.ru/api_jsonrpc.php', 'script', '123123');
 var hostid = '10211';
 
+// Подключение конфига
+var config = require('./config.json');
+// // Использование конфига
+console.dir(config.port);
+
+
 // Should be call login at the first time
-client.login(function(error, resp, body) {
+/*client.login(function(error, resp, body) {
     client.call('host.get', {'hostids': hostid}, function(error, resp, body) {
       console.log(body[0].host);
     });
 });
+*/
 
 /* // Then the client has had the token
 client.call('host.get', {'hostids': hostid}, function(error, resp, body) {
