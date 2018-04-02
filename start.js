18bcb753a840461d6b205be380f1aafb2fe00654
scode <scode@bk.ru>
2018-04-02 23:54:30 +0700

    пн 2

--- start.js
+++ start.js
@@ -1,3 +1,7 @@
+//----tips
+//process.stdout.write(d);
+
+
 var zabbixNode = require("zabbix-node");
 var getJSON = require("get-json");
 var mysql = require("mysql2/promise");
@@ -5,20 +9,34 @@ const dedent = require("dedent");
 var moment = require("moment");
 const phin = require("phin").promisified;
 var get = require("lodash.get");
-// Подключение конфига
-var config = require("./config.json");
-//--------------------------------------
-/*
-fix
+
+
+const https = require("https");
+/*------<fix>-----------------------
+не забирает данные с сайтов с косяным сертификатом. в возникшей ошибке причина -не включен премежуточный сертификат в файл сертификата полученного с сервера
+проверка сертификата:
 https://www.ssllabs.com/ssltest/analyze.html?d=graviex.net
+текст ошибки:
 Unexpected error occurred in createConnection { Error: unable to verify the first certificate
     at TLSSocket.onConnectSecure (_tls_wrap.js:1036:34)
     at TLSSocket.emit (events.js:127:13)
     at TLSSocket._finishInit (_tls_wrap.js:633:8) code: 'UNABLE_TO_VERIFY_LEAF_SIGNATURE' }
-*/
-//--------------------------------------
+    в коде ниже мы подключаем библиотеку, которая загружает свежие сертификаты, которым доверяют браузеры но не доверяет нода, плюс мы ложем недостающий промежутоынй сертификат*/
+var rootCas = require("ssl-root-cas/latest").create();
+let outs = rootCas.addFile(
+  __dirname + "/config/ssl/comodorsadomainvalidationsecureserverca.crt"
+);
+https.globalAgent.options.ca = rootCas;
+//------</fix>-----------------------
 
-test(Done);
+// Подключение конфига
+var config = require("./config.json");
+
+
+
+
+//test(Done);
+getTiker(1,Done);
 
 var client = new zabbixNode(
   "http://zbxs.e5-nsk.ru/api_jsonrpc.php",
@@ -84,20 +102,6 @@ function asyncgetJSON(url) {
 }
 
 async function writeData(cb, currency_id, ondate, balance, balance_immature) {
-  let connection;
-  try {
-    connection = await mysql.createConnection({
-      host: config.sql.host,
-      user: config.sql.user,
-      password: config.sql.password,
-      database: config.sql.database
-    });
-  } catch (e) {
-    return cb(
-      "Unexpected error occurred in createConnection during writeData processing - ",
-      e.message
-    );
-  }
 
   //console.log("DB connected");
   /*var [rows, fields] = await connection.execute('show databases');
@@ -179,7 +183,7 @@ async function test(cb) {
       //console.log("headers:", res.headers);
 
       res.on("data", d => {
-        process.stdout.write(d);
+        //process.stdout.write(d);
       });
     })
     .on("error", e => {
@@ -271,4 +275,83 @@ async function test(cb) {
     return cb("Unexpected error occurred in createConnection", e);
   }
   let balance = res.body.wallet_balance;*/
+}
+
+
+async function getTiker (cb,id) {
+
+  //db.execute('SELECT * FROM users WHERE LIMIT = ?', [10]).spread(function (users) {
+   // console.log('Hello users', users);
+
+  let TikersData = {
+    PROTON: {
+      url: "https://graviex.net//api/v2/tickers/protonbtc.json",
+      last: "",
+      volume: "",
+      ask: "",
+      bid: "",
+      chek: ""
+    },
+    ULT: {
+      url: "https://api.crypto-bridge.org/api/v1/ticker",
+      last: "[14].last",
+      volume: "",
+      ask: "",
+      bid: "",
+      chek: "id=ULT_BTC"
+    }
+  };
+
+  let res = {};
+  let url = "https://graviex.net//api/v2/tickers/protonbtc.json";
+  try {
+    //var restp = await asyncgetJSON(url);
+    res = await phin({
+      url: url,
+      parse: "json"
+    });
+
+    let last = get(res, "body.ticker.last");
+    let volume = get(res, "body.ticker.volbtc");
+    let ask = get(res, "body.ticker.sell");
+    let bid = get(res, "body.ticker.buy");
+    console.log("last:", last);
+    console.log("volume:", volume);
+    console.log("ask:", ask);
+    console.log("bid:", bid);
+  } catch (e) {
+    return cb("Unexpected error occurred in createConnection", e);
+  }
+  
+}
+
+
+const start = async () => {
+  // do something
+  let connection;
+  DBconnect(this.connection);
+  if (!connection){
+    console.log('ca')
+  }
+
+}
+
+
+
+async function DBconnect(connection)
+{  
+  try {
+    connection = await mysql.createConnection({
+      host: config.sql.host,
+      user: config.sql.user,
+      password: config.sql.password,
+      database: config.sql.database
+    });
+  } catch (e) {
+    return cb(
+      "Unexpected error occurred in createConnection during writeData processing - ",
+      e.message
+    );
+  }
+
 }
